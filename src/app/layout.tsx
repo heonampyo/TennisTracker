5aeb314281cc48752322729d5c6bc950ed043f46
허남표 <nampyo.heo@kbfg.com>
2024-11-01 22:57:36 +0900

    메타데이타 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,13 +6,13 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: '파새단(파주 새벽 단식 테니스)',
-  description: '주경기장 : 파주 상지석 테니스장 새벽 6시',
+  description: '파주 상지석 테니스장 새벽 6시',
   openGraph: {
     title: '파새단(파주 새벽 단식 테니스)',
-    description: '주경기장 : 파주 상지석 테니스장 새벽 6시',
+    description: '파주 상지석 테니스장 새벽 6시',
     images: [
       {
-        url: '../public/pasaedan.jpg', // public 폴더에 있는 이미지 경로
+        url: 'https://tennis-tracker-azure.vercel.app/pasaedan.jpg', // public 폴더에 있는 이미지 경로
         width: 1200,
         height: 630,
         alt: '테니스공 이미지',
